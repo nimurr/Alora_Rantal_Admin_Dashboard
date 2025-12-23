a21b607dca36b8b3028b75e3b104ce9226b1dff2
nimurr <nimurnerob404@gmail.com>
2025-12-23 09:59:13 +0600

    `Added FaPhone and MdEmail icons, updated apartment room number, and added owner's contact information`

--- src/page/MaintenceCrew/MaintenceCrewViewWorkHistoryDetails.jsx
+++ src/page/MaintenceCrew/MaintenceCrewViewWorkHistoryDetails.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { FaAngleLeft } from 'react-icons/fa'
+import { FaAngleLeft, FaPhone } from 'react-icons/fa6'
+import { MdEmail } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
 export default function MaintenceCrewViewWorkHistoryDetails() {
     return (
         <div>
-            <Link to="/roles/apartment-owner" className="flex my-5 items-center gap-2">
+            <Link to="/roles/maintence-crew" className="flex my-5 items-center gap-2">
                 <FaAngleLeft className="text-2xl" />
                 <span className="text-2xl font-semibold">Details</span>
             </Link>
             <div className='grid xl:grid-cols-3 gap-5'>
-                <div className='border col-span-2 rounded-lg border-gray-200 p-5'>
+                <div className='border lg:col-span-2 rounded-lg border-gray-200 p-5'>
                     <span className='text-center block'>Apartment Room number</span>
                     <h2 className='text-center block font-semibold mt-2 text-xl'>BG101 (1st floor)</h2>
                     <div className='flex items-center justify-between border-b pb-4 mt-4'>
@@ -60,12 +61,30 @@ export default function MaintenceCrewViewWorkHistoryDetails() {
                             <span>Sat 12 April 2025  8.30 PM </span>
                         </div>
                         <div className='flex  gap-3 mt-5'>
-                            <img className='w-8 h-8 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnFRPx77U9mERU_T1zyHcz9BOxbDQrL4Dvtg&s" alt="" />
+                            <img className='w-10 h-10 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnFRPx77U9mERU_T1zyHcz9BOxbDQrL4Dvtg&s" alt="" />
                             <div>
-                                <h2 className='font-semibold'>John Doe</h2>
-                                <span className='text-sm'>Maintence Crew</span>
+                                <h2 className='font-semibold'>name</h2>
+                                <span className='text-sm'>John Doe</span>
                             </div>
                         </div>
+                        <div className='flex  gap-3 mt-5'>
+                            <MdEmail className='text-2xl text-gray-400' />
+                            <div>
+                                <h2 className='font-semibold'>email</h2>
+                                <span className='text-sm'>support@gmail.com</span>
+                            </div>
+                        </div>
+                        <div className='flex  gap-3 mt-5'>
+                            <FaPhone className='text-2xl text-gray-400' />
+                            <div>
+                                <h2 className='font-semibold'>phone number</h2>
+                                <span className='text-sm'>0178000000</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='border p-5 rounded-lg mt-5'>
+                        <span className='block mb-2 font-semibold'>Resolved By Owner</span>
+                        <p className='text-gray-500 mt-1'>Resolving issue date <br /> Sat 12 April 2025  8.30 PM </p>
                     </div>
                 </div>
             </div>
